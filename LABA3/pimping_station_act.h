dfa185a94f51ff537daef6b62060c70fbd2ebe9f
Egor Shakin <redzone14@mail.ru>
2025-05-27 13:56:57 +0300

    pumping_station_act.h

---
+++ LABA3/pimping_station_act.h
@@ -0,0 +1,9 @@
+#pragma once 
+#include "pumping_station.h" 
+#include <unordered_map> 
+
+// Function declarations 
+void addPumpingStation(std::unordered_map<int, PumpingStation>& pumpingStations);
+void viewPumpingStations(const std::unordered_map<int, PumpingStation>& pumpingStations);
+void modifyPumpingStationStatus(std::unordered_map<int, PumpingStation>& pumpingStations);
+void deletePumpingStation(std::unordered_map<int, PumpingStation>& pumpingStations);
